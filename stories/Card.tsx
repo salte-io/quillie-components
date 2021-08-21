97e7c3d6bc33a064d678b11f1ec5430390e2e842
Cecilia Woodward <ceci@salte.io>
2021-08-21 11:15:53 -0500

    fix(Card): added a loading property

--- stories/Card.tsx
+++ stories/Card.tsx
@@ -3,29 +3,41 @@ import React, { ReactNode } from 'react';
 import styles from './Card.scss';
 import { Layout } from './constants';
 import { Grid } from './Grid';
+import { Spinner } from './Spinner';
 
 export interface CardProps {
   children?: ReactNode;
   className?: string;
   layout?: Layout;
+  loading?: boolean;
 }
 
 export function Card({
   children,
   className,
   layout = Layout.Vertical,
+  loading = false,
   ...props
 }: CardProps): JSX.Element {
   return (
-    <Grid
+    <div
       className={classNames(
         styles.card,
+        loading && styles.loading,
         className
       )}
-      layout={layout}
       {...props}
     >
-      {children}
-    </Grid>
+      <Grid
+        className={styles.content}
+        layout={layout}
+      >
+        {children}
+      </Grid>
+      <Spinner
+        className={styles.spinner}
+        loading={loading}
+      />
+    </div>
   );
 }
