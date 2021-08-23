cc1d4a2dc4af2494700431feb6adb695f35f8158
Cecilia Woodward <ceci@salte.io>
2021-08-23 17:38:16 -0500

    fix(Tooltip): added a basic tooltip

---
+++ stories/Tooltip.tsx
@@ -0,0 +1,56 @@
+import classNames from 'classnames';
+import React, { ReactNode, useEffect, useState } from 'react';
+import { Alignment } from './constants';
+
+import styles from './Tooltip.scss';
+
+export interface TooltipProps {
+  align?: Alignment;
+  children: ReactNode;
+  id: string;
+}
+
+export interface TooltipState {
+  visible: boolean;
+}
+
+export function Tooltip({
+  align = Alignment.Right,
+  children,
+  id,
+}: TooltipProps): JSX.Element {
+  const [visible, setVisible] = useState<boolean>();
+
+  useEffect(() => {
+    const element = document.querySelector<HTMLElement>(`[data-tooltip="${id}"]`);
+
+    if (visible) {
+      element.addEventListener('mouseleave', () => {
+        setVisible(false);
+      }, {
+        once: true,
+        passive: true,
+      });
+    } else {
+      element.addEventListener('mouseenter', () => {
+        setVisible(true);
+      }, {
+        once: true,
+        passive: true,
+      });
+    }
+  }, [visible]);
+
+
+  return (
+    <div
+      className={classNames(
+        styles.tooltip,
+        styles[align],
+        visible && styles.visible,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
