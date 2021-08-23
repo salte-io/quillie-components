5ceebda883b02b6486e8483c94078c787345c88f
Ceci <admin@cecilias.me>
2021-08-23 16:05:18 -0500

    fix(Spinner): support not being fit and other colors

--- stories/Spinner.tsx
+++ stories/Spinner.tsx
@@ -1,22 +1,29 @@
 import classNames from 'classnames';
 import React from 'react';
+import { Theme } from './constants';
 
 import styles from './Spinner.scss';
 
 export interface SpinnerProps {
   className?: string;
   loading?: boolean;
+  fit?: boolean;
+  theme?: Theme;
 }
 
 export function Spinner({
   className,
   loading = false,
+  fit = false,
+  theme = Theme.Primary,
 }: SpinnerProps): JSX.Element {
   return (
     <div
       className={classNames(
         styles.spinner,
         loading && styles.loading,
+        fit && styles.fit,
+        styles[theme],
         className,
       )}
     />
