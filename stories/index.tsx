fc91095e8e6e20a9685dd8d252c585470870a8ef
Cecilia Woodward <ceci@salte.io>
2021-08-24 14:03:34 -0500

    fix(Icon): export types

--- stories/index.tsx
+++ stories/index.tsx
@@ -3,7 +3,7 @@ export { Actions } from './Actions';
 export { Button } from './Button';
 export { Card } from './Card';
 export { Checkbox } from './Checkbox';
-export { Icon } from './Icon';
+export { Icon, IconType } from './Icon';
 export { Input } from './Input';
 export { Grid } from './Grid';
 export { Spinner } from './Spinner';
