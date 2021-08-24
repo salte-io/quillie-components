a275388256acf319e9b40e7f1708ceaf6add09de
Ceci <admin@cecilias.me>
2021-08-23 22:10:02 -0500

    fix(Toast): export the add function

--- stories/index.tsx
+++ stories/index.tsx
@@ -11,4 +11,4 @@ export { Favorite } from './Favorite';
 export { Separator } from './Separator';
 export { Tooltip } from './Tooltip';
 export { Typography, TypographyStyles } from './Typography';
-export { Toasts } from './Toasts';
+export { Toasts, AddToastNotification } from './Toasts';
